9bbc3ecbe7a0e323cee6fa7dae826d771f71188b
hoangdq <hoangdq.vnist@gmail..com>
2020-05-29 15:51:50 +0700

    Finish git add .

---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+const loginForm = document.getElementById('login-form');
+const socket = io();
+
+// Check user name is unique
+function logSubmit(e) {
+  // Get username from form
+  const username = e.target.elements.username.value;
+
+  // Tell backend we have a new user request
+  socket.emit('newUser', username);
+
+  // Backend confirms user is unique (let them continue)
+  socket.on('uniqueUser', (message) => {
+    console.log('user is unique');
+  });
+
+  // User is not unique, send to home page
+  socket.on('duplicateUser', (message) => {
+    window.location = 'http://localhost:3000/index.html';
+  });
+}
+
+loginForm.addEventListener('submit', logSubmit);
